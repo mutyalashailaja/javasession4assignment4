d9c9e838f56d3ee407c1d2eedc8a4cffaced62e4
mutyalashailaja <mutyala.shailaja@gmail.com>
2016-08-27 08:12:41 +0530

    session4assignment4

---
+++ src/reversestring.java
@@ -0,0 +1,13 @@
+
+public class reversestring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StringBuffer buffer = new StringBuffer("shailaja");
+		 buffer.reverse();
+		 System.out.println(buffer);
+
+	}
+
+}
